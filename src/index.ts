87027378f2482e171769b0fdcf2c9356686fd755
Jonathan Rodrigues <jonarc06@gmail.com>
2021-10-22 21:07:41 -0300

    waiting of  min 3000ms

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,26 @@ import { getXlsxData } from "./getXlsxData";
 import { replaceStakeholders } from "./replaceStakeHolders";
 
 const main = async () => {
+  console.log(`
+    Idealizado por: Leandro Oliveira || https://www.instagram.com/chirodinguer/
+    Desenvolvidor por: Jonathan Rodrigues || https://www.linkedin.com/in/jonathan-rodrigues-cardoso/
+  `);
+
+  const waiting = wait(3000);
+
   const data = await getXlsxData();
 
   const { savarComo: outPath } = await getConfig();
 
-  replaceStakeholders(data, outPath);
+  await replaceStakeholders(data, outPath);
+
+  await waiting;
 };
 
-main().catch(() => {
-  process.exit(1);
+main().catch((error) => {
+  console.log("Erro inesperado");
+  setTimeout(() => process.exit(1), 3000);
 });
+
+const wait = (time: number) =>
+  new Promise((resolve) => setTimeout(resolve, time));
